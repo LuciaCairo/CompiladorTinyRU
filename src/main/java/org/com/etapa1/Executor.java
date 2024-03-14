7d883c2aa4ba2783404d254443339b8977cf7c91
LuciaCairo <luciicairo@gmail.com>
2024-03-14 18:37:05 -0300

    get tokens

--- src/main/java/org/com/etapa1/Executor.java
+++ src/main/java/org/com/etapa1/Executor.java
@@ -1,81 +1,70 @@
 package org.com.etapa1;
-import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.FileWriter;
+
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Executor {
     public static void main(String[] args) {
-
-        AnalizadorLexico aL = new AnalizadorLexico();
-        /* Variable para decidir si los tokens seran impresos por consola o por un archivo de salida
-        - si se proporciona un 3er argumento: sera el nombre del archivo de salida .txt
-        - si no hay 3er argumento, la salida sera impresa por pantalla */
+        AnalizadorLexico l = new AnalizadorLexico();
         boolean printToFile = (args.length == 2);
+        String title = "| TOKEN | LEXEMA | NUMERO DE LINEA (NUMERO DE COLUMNA)|\n";
 
-        String title= "| TOKEN | LEXEMA | NUMERO DE LINEA (NUMERO DE COLUMNA)|\n";
-
-        // Salida: archivo.txt
-        if(!printToFile) {
-
-            String rutaDirectorio = System.getProperty("user.dir");
-            String rutaArchivo = rutaDirectorio + File.separator + "\\src\\main\\java\\org\\com\\etapa1\\salida.txt";
-
-            try {
-                System.out.print(rutaArchivo);
-                File archivo = new File(rutaArchivo);
-                FileWriter escritorArchivo = new FileWriter(archivo);
-                BufferedWriter escritorBuffer = new BufferedWriter(escritorArchivo);
-                escritorBuffer.write("CORRECTO: ANALISIS LEXICO\n");
-                escritorBuffer.write(title);
-                //escritorBuffer.close(); // ESTO NOSE SI ESTA BIEN QUE CIERRE
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }else { // Salida: impresion por pantalla
-
-            System.out.print("CORRECTO: ANALISIS LEXICO\n");
-            System.out.print(title);
-        }
         try {
-            aL.analizarArchivo("C:\\Users\\Agustina\\Desktop\\CompiladorTinyRU\\src\\main\\java\\org\\com\\etapa1\\prueba.ru");
+            BufferedWriter writerBuffer = null;
 
-            Token t = aL.nextToken();
-            System.out.println(t.getName());
+            if (printToFile) {
+                writerBuffer = createdBuffer();
+                writerBuffer.write("CORRECTO: ANALISIS LEXICO\n");
+                writerBuffer.write(title);
+            } else {
+                System.out.print("CORRECTO: ANALISIS LEXICO\n");
+                System.out.print(title);
+            }
 
-        } catch (Exception e) {
+            l.analizarArchivo("C:\\Users\\Luci\\Documents\\Ciencias de la Computacion\\Compiladores\\CompiladorTinyRU\\src\\main\\java\\org\\com\\etapa1\\prueba.ru");
+
+            if (printToFile) {
+                printTokens(writerBuffer, l);
+            } else {
+                printTokensConsola(l);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
-
         }
-        /*try {
-            while(aL.hasNextToken()) {
-                Token actualToken = aL.nextToken();
-                linea = "| "+ actualToken.getpReservada() +
-                        " | "+ actualToken.getValor()+
-                        " | "+ actualToken.getFila() + " |\n";
-                if(!printToFile) {
-                    System.out.print(linea);
+    }
 
-                }else {
-                    impresora.write(linea);
-                }
-            }
-        }catch (Exception e) {
-            e.printStackTrace();
+    private static BufferedWriter createdBuffer() throws IOException {
+        String router = getRoute();
+        FileWriter writerArchivo = new FileWriter(router);
+        return new BufferedWriter(writerArchivo);
+    }
 
-        }finally {
-            if(printToFile) {
-                try {
-                    impresora.close();
-                }catch(Exception e2) {
-                    e2.printStackTrace();
-                }
-            }
-        }*/
+    private static String getRoute() {
+        String routeDirectorio = System.getProperty("user.dir");
+        return routeDirectorio + File.separator + "\\src\\main\\java\\org\\com\\etapa1\\salida.txt";
+    }
+
+    private static void printTokens(BufferedWriter writerBuffer, AnalizadorLexico l) throws IOException {
+        while (l.countTokens() > 0) {
+            Token t = l.nextToken();
+            String text = "| " + t.getName() +
+                    " | " + t.getLexema() +
+                    " | LINEA " + t.getLine() + " (COLUMNA " + t.getCol() + ") |\n";
+            writerBuffer.write(text);
+        }
+        writerBuffer.close();
+    }
+
+    private static void printTokensConsola(AnalizadorLexico l) {
+        while (l.countTokens() > 0) {
+            Token t = l.nextToken();
+            String text = "| " + t.getName() +
+                    " | " + t.getLexema() +
+                    " | LINEA " + t.getLine() + " (COLUMNA " + t.getCol() + ") |\n";
+            System.out.print(text);
+        }
     }
 }
 
